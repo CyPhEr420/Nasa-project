39ec3d8b2911dbcaf11d2a42bad738d0be3e89ac
CyPhEr420 <vimalvk9900@gmail.com>
2022-06-20 13:10:23 +0530

    added dotenv

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const MONGO_URL = process.env.MONGO_URL;
+
 
 
 mongoose.connection.once('open', () => {
@@ -13,8 +15,10 @@ mongoose.connection.on('error', (err) => {
 
 
 async function mongoConnect() {
+
     await mongoose.connect(MONGO_URL);
 }
+
 
 async function mongoDisconnect() {
     await mongoose.disconnect();
